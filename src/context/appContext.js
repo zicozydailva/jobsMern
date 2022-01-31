967cf529eb068ccfde8dc57f60b5ee9bceb0c2d2
Ogbu Ezekiel Ike <zico@Macbooks-MacBook-Pro.local>
2022-01-31 22:32:36 +0100

    setting up the state ..contextAPI

---
+++ src/context/appContext.js
@@ -0,0 +1,24 @@
+import { useState, useContext, useReducer, createContext } from "react";
+
+const initialState = {
+  isLoading: false,
+  showAlert: false,
+  alertText: "",
+  alertType: "",
+};
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, setState] = useState(initialState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, initialState, useAppContext };
