ec18e29508c26a699544d304aac8b4f8603ab0e6
Ogbu Ezekiel Ike <zico@Macbooks-MacBook-Pro.local>
2022-02-01 17:07:02 +0100

    connecting to api

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/v1")
+      const data = await response.json()
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return <div>Dashboard Page</div>;
 }
 
 export default Dashboard;
