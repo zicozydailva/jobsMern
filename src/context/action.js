a4aba61df6e136d9c1e8cfd3f478d2bb549efaf3
Ogbu Ezekiel Ike <zico@Macbooks-MacBook-Pro.local>
2022-02-01 17:24:11 +0100

    actions update

--- src/context/action.js
+++ src/context/action.js
@@ -1,2 +1,5 @@
 export const DISPLAY_ALERT = "SHOW_ALERT"
 export const CLEAR_ALERT = "CLEAR_ALERT"
+export const REGISTER_USER = "REGISTER_USER"
+export const SUCCESS = "SUCCESS"
+export const ERROR = "ERROR"
